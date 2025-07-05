271c871891608c9ae9f62c40cc24e84a226c2b73
I8P010 <ivvo0102@gmail.com>
2025-07-06 00:41:27 +0300

    Added Post class.
    It contains data about post. It's still without documentation.

---
+++ Core/Models/Post.cs
@@ -0,0 +1,32 @@
+namespace I8Pages.Core;
+
+/// <summary>
+/// 
+/// </summary>
+public class Post
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public Guid Id { get; }
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public string Title { get; }
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public string Text { get; }
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public DateTime CreationTime { get; }
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public DateTime LastUpdateTime { get; }
+}
