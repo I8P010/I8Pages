2293fb1a1a40812c95b183fcf29f9a2ae46f0202
I8P010 <ivvo0102@gmail.com>
2025-07-06 20:51:20 +0300

    Some small changes

--- Core/Models/Interfaces/IStorageProvider.cs
+++ Core/Models/Interfaces/IStorageProvider.cs
@@ -1,17 +1,44 @@
+using I8Pages.Core.Utilities;
+
 namespace I8Pages.Core;
 
 /// <summary>
-/// 
+/// An interface that represents the provider for storing blog posts files.
 /// </summary>
 public interface IStorageProvider
 {
-	Task<string> CreateAsync(string title);
+	/// <summary>
+	/// Creates a new post. Returns the path of the new post's file.
+	/// </summary>
+	/// <param name="title">The title of the post.</param>
+	/// <param name="id">The ID of the new post.</param>
+	/// <returns>An object representing an asynchronously running operation.</returns>
+	Task<string> CreateAsync(Guid id, string title);
 
+	/// <summary>
+	/// Gets the data of a post by its ID.
+	/// </summary>
+	/// <param name="id">The ID of the post.</param>
+	/// <returns>An object representing an asynchronously running operation.</returns>
 	Task<GetData> GetAsync(Guid id);
-	
+
+	/// <summary>
+	/// Gets IDs of all posts.
+	/// </summary>
+	/// <returns>An object representing an asynchronously running operation.</returns>
 	Task<IEnumerable<Guid>> GetIdsAsync();
 
+	/// <summary>
+	/// Deletes a post by its ID.
+	/// </summary>
+	/// <param name="id">The ID of the post.</param>
+	/// <returns>An object representing an asynchronously running operation.</returns>
 	Task DeleteAsync(Guid id);
 
+	/// <summary>
+	/// Writes information about a post represented as an object of type <c>Post</c> to a file corresponding to this post.
+	/// </summary>
+	/// <param name="post">A post represented with the <c>Post</c> type.</param>
+	/// <returns>An object representing an asynchronously running operation.</returns>
 	Task SaveAsync(Post post);
 }
--- Core/Utilities/GetData.cs
+++ Core/Utilities/GetData.cs
@@ -1,4 +1,3 @@
 namespace I8Pages.Core.Utilities;
 
-// TODO: Переименовать получше
 public record GetData(string Title, string Text, DateTime UtcCreationTime, DateTime UtcLastUpdateTime, string Path);
